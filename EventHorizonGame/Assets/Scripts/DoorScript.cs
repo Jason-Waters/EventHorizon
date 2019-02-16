c67f0a83bcbc8575042007feb5f80df3ccb354ec
Jason-Waters <42689149+Jason-Waters@users.noreply.github.com>
2019-02-16 16:54:21 -0500

    Added Project

---
+++ EventHorizonGame/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+
+    public Animator anim;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player") || other.CompareTag("Alien"))
+        {
+            anim.SetBool("playOpen", true);
+            anim.SetBool("isOpen", false);
+            anim.SetBool("isClosed", true);
+        }
+    }
+
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Alien"))
+        {
+
+            anim.SetBool("isOpen", true);
+            anim.SetBool("playOpen", false);
+
+
+
+        }
+    }
+}
---
+++ EventHorizonGame/Assets/Scripts/Shoot.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+
+    public Transform gunEnd;
+    private float fireRate = .25f;
+    public float weaponRange = 50f;
+
+    private LineRenderer laserLine;
+    private float nextFire;
+    private Camera fpsCam;
+
+    private RaycastHit hit;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserLine = GetComponent<LineRenderer>();
+        fpsCam = GetComponentInParent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+
+            StartCoroutine(ShotEffect());
+
+            //viewport is 1,1. 0.5,0.5 is center of camera
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+
+
+            //set position of starting line at end of the gun.
+            laserLine.SetPosition(0, gunEnd.position);
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, weaponRange))
+            {
+                laserLine.SetPosition(1, hit.point);                    
+                
+            }
+            else
+            {
+                laserLine.SetPosition(1, rayOrigin + (fpsCam.transform.forward * weaponRange));
+            }
+        }
+    }
+    
+
+
+
+    private IEnumerator ShotEffect()
+    {
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}

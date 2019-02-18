c51d648719deaadbde0b21715c4449d93e239600
dprasad1027 <36938476+dprasad1027@users.noreply.github.com>
2019-02-18 08:24:07 -0500

    Gun Effects 1

--- EventHorizonGame/Assets/Scripts/Gun.cs
+++ EventHorizonGame/Assets/Scripts/Gun.cs
@@ -7,8 +7,13 @@ public class Gun : MonoBehaviour
     public float damage = 10f;
     public float range = 100f;
     public LayerMask targetLayer;
-
+    public LayerMask obstacleLayer;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
     public Camera fpsCam;
+    public float fireRate = 2f;
+
+    private float nextTimeToFire = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,8 +24,9 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
         {
+            nextTimeToFire = Time.time + 1f / fireRate;
             Shoot();
 
         }
@@ -29,7 +35,10 @@ public class Gun : MonoBehaviour
 
     private void Shoot()
     {
+        muzzleFlash.Play();
         RaycastHit hit;
+
+        
         
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range,targetLayer))
         {
@@ -39,6 +48,14 @@ public class Gun : MonoBehaviour
             {
                 alien.GetStun(true);
             }
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
+
+
+        }else if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range, obstacleLayer))
+        {
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
         }
     }
 }

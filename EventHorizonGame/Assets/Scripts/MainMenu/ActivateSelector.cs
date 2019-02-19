75bcdee0a0d7273550051e3ec478187e4e3289a0
dprasad1027 <36938476+dprasad1027@users.noreply.github.com>
2019-02-18 22:44:31 -0500

    Fixed Menu Stuff

---
+++ EventHorizonGame/Assets/Scripts/MainMenu/ActivateSelector.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateSelector : MonoBehaviour
+{
+    public void TurnOnSelector()
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+    }
+
+    public void TurnOffSelector()
+    {
+        gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    }
+
+}
---
+++ EventHorizonGame/Assets/Scripts/MainMenu/ButtonAudio.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAudio : MonoBehaviour
+{
+
+    public AudioSource myFx;
+    public AudioClip hoverFx;
+    public AudioClip clickFx;
+
+
+    public void HoverSound()
+    {
+        myFx.PlayOneShot(hoverFx);
+    }
+
+    public void ClickSound()
+    {
+        myFx.PlayOneShot(clickFx);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
